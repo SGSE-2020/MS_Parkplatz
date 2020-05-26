17816640b54e0fe7a992ce525b5ae0ee9c9cf610
UnlikePluto <sv.simikin@gmail.com>
2020-05-26 23:10:50 +0200

    Provide sign-in and sign-out functionality

---
+++ frontend/ParkSpace/src/app/services/authentication/authentication.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {Router} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor(private angularFireAuth: AngularFireAuth,
+              private router: Router) {
+  }
+
+  public get currentUserToken() {
+    return this.angularFireAuth.currentUser;
+  }
+
+  login(email, password) {
+    return this.angularFireAuth.signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        return result.user.getIdToken(true);
+      });
+  }
+
+  logout() {
+    this.angularFireAuth.signOut();
+    this.router.navigate(['/login']);
+  }
+}
