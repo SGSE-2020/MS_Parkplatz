624f13bf9ecf0290c538231fe441b865c7303a0e
UnlikePluto <sv.simikin@gmail.com>
2020-05-26 23:07:33 +0200

    Define firebase requirements and rest api dummy url

---
+++ frontend/ParkSpace/src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+
+  restApi: 'http://' + 'localhost' + ':' + '8081',
+  restApiVersion: 'v1',
+
+  firebase: {
+    apiKey: 'AIzaSyBvTg0_QrhEvQ9UeZPH8--E2JZ55KA_u_c',
+    authDomain: 'smart-city-ss2020.firebaseapp.com',
+    databaseURL: 'https://smart-city-ss2020.firebaseio.com',
+    projectId: 'smart-city-ss2020',
+    storageBucket: 'smart-city-ss2020.appspot.com',
+    messagingSenderId: '957240233717'
+  }
+};
