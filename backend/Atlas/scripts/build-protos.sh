22d527082faea56cf29f746490bc5b97b1b355e3
UnlikePluto <sv.simikin@gmail.com>
2020-05-27 16:16:00 +0200

    Add protocol buffer definition and creation scripts

---
+++ backend/Atlas/scripts/build-protos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# JavaScript code generation
+grpc_tools_node_protoc --js_out=import_style=commonjs,binary:./src/proto --grpc_out=./src/proto --proto_path=./protos parkplatz.proto
+
+# TypeScript code generation
+grpc_tools_node_protoc --ts_out=import_style=commonjs,binary:./src/proto --grpc_out=./src/proto --proto_path=./protos parkplatz.proto
