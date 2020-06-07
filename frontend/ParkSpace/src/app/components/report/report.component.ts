d19db1f3eb932855314ff7c1d881e674eaa76821
UnlikeMars <37548459+UnlikeMars@users.noreply.github.com>
2020-06-07 22:23:26 +0200

    Add report page sample content

--- frontend/ParkSpace/src/app/components/report/report.component.ts
+++ frontend/ParkSpace/src/app/components/report/report.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {MatCheckboxChange} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-report',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
-  constructor() { }
+  form = this.fb.group({
+    textArea: ['', [Validators.required]],
+  });
+  selectedReservation: any;
+  reservations = [
+    {
+      id: '123-456-ABC',
+      date: 1558343923,
+      displayName: 'Waldparkplatz'
+    },
+    {
+      id: '123-ABC-456',
+      date: 1588343923,
+      displayName: 'Parkplatz am See'
+    }
+  ];
+
+  constructor(private fb: FormBuilder,) {
+  }
 
   ngOnInit(): void {
   }
 
+  submit() {
+
+  }
+
+  consentTermChange($event: MatCheckboxChange) {
+
+  }
+
+  consentLegal($event: MatCheckboxChange) {
+
+  }
 }
