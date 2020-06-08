3df91b6a9578d54428114680fe9504a6a0990a0c
UnlikeMars <37548459+UnlikeMars@users.noreply.github.com>
2020-06-08 20:40:21 +0200

    Removed unused user service

--- backend/Atlas/src/services/interface/IUserService.ts
+++
@@ -1,7 +0,0 @@
-import {Service} from "../Service";
-import {ReservationEntity} from "../../models/entity/ReservationEntity";
-import {ReservationDTO} from "../../models/dto/ReservationDTO";
-
-export interface IUserService extends Service<ReservationEntity> {
-    getInformation(reservationId: string): Promise<ReservationEntity>;
-}
