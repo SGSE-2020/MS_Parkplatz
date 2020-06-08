e94d7316c87fedaca4603c53555c1e2794a0bdd7
UnlikeMars <37548459+UnlikeMars@users.noreply.github.com>
2020-06-08 20:39:20 +0200

    Intercept bearer token and acquire user id

--- backend/Atlas/src/server/handler/TokenInterceptor.ts
+++ backend/Atlas/src/server/handler/TokenInterceptor.ts
@@ -1,9 +1,10 @@
-// import {UserServiceClient} from "../../proto/user_grpc_pb";
-// import * as grpc from "grpc";
-// import {UserToken} from "../../proto/user_pb";
 import * as errors from "restify-errors";
+import {civilOfficeGrpc} from "../../enviroment";
+import * as grpc from "grpc";
+import {UserServiceClient} from "../../proto/user_grpc_pb";
+import {UserId, UserToken} from "../../proto/user_pb";
 
-export function tokenInterceptor(req: any, res: any, next: any) {
+export async function tokenInterceptor(req: any, res: any, next: any) {
     const bearer = req.header('Authorization');
     if (bearer === undefined) {
         return next(new errors.UnauthorizedError('\'Authorization\' header not provided.'));
@@ -17,20 +18,44 @@ export function tokenInterceptor(req: any, res: any, next: any) {
     if (bearerSplit[0] !== 'Bearer') {
         return next(new errors.UnauthorizedError('\'Authorization\' header \'Bearer\' not provided.'));
     }
-
-    // const userToken = new UserToken();
-    // userToken.setToken(bearerSplit[1])
-
-    //FIXME this is here for test purposes only
-    // const userServiceClient = new UserServiceClient('localhost:9000', grpc.credentials.createInsecure());
-    // userServiceClient.verifyUser(userToken, function (err, response) {
-    //     if (err.code.valueOf() === 14) {
-    //         console.log('ERROR: ' + err.message);
-    //         console.log('ERROR: gRPC user.proto backend not available');
-    //     } else {
-    //         console.log('uid:', response.getUid());
-    //     }
-    // });
+    try {
+        const result = await gRpcWrapper(bearerSplit[1])
+        req.userUid = (<UserId>result).getUid();
+    } catch (error) {
+        return next(error);
+    }
 
     return next();
+}
+
+function gRpcWrapper(query) {
+    return new Promise((resolve, reject) => {
+        gRpcVerify(query, (successResponse) => {
+            resolve(successResponse);
+        }, (errorResponse) => {
+            reject(errorResponse)
+        });
+    });
+}
+
+function gRpcVerify(tokenString, successCallback, errorCallback) {
+    const userToken = new UserToken();
+    userToken.setToken(tokenString)
+
+    const userServiceClient = new UserServiceClient(civilOfficeGrpc, grpc.credentials.createInsecure());
+    userServiceClient.verifyUser(userToken, function (err, response) {
+        if (err !== null) {
+            console.log(err);
+
+            if (err.code == 14) {
+                errorCallback(new errors.UnauthorizedError('Invalid \'Bearer\' token.'));
+            } else if (err.code == 2) {
+                errorCallback(new errors.InternalServerError('Backend Civil-Office not reachable.'));
+            } else {
+                errorCallback(new errors.InternalServerError(err));
+            }
+        } else {
+            successCallback(response)
+        }
+    });
 }
