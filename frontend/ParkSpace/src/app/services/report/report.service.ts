1d195ddb0daba945b550f00a3818427f12570bad
UnlikeMars <37548459+UnlikeMars@users.noreply.github.com>
2020-06-30 12:18:33 +0200

    Fix observable not observed and therefore post not executed

--- frontend/ParkSpace/src/app/services/report/report.service.ts
+++ frontend/ParkSpace/src/app/services/report/report.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {ReportModel} from '../../models/report.model';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class ReportService {
   constructor(private httpClient: HttpClient) {
   }
 
-  postReport(reportModel: ReportModel) {
+  postReport(reportModel: ReportModel): Observable<any> {
     const href = `${environment.restApi}/reports`;
     const requestUrl = `${href}`;
     return this.httpClient.post(requestUrl, reportModel);
