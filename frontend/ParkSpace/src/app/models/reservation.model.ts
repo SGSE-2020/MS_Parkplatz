cf03b34d199b027b21da740a65aa150a8e81e75e
UnlikeMars <37548459+UnlikeMars@users.noreply.github.com>
2020-06-30 11:00:39 +0200

    Extend the reservation model by area id and reservation id

--- frontend/ParkSpace/src/app/models/reservation.model.ts
+++ frontend/ParkSpace/src/app/models/reservation.model.ts
@@ -1,7 +1,10 @@
 export interface ReservationModel {
+  reservation_id: string;
   start_timestamp: number;
   end_timestamp: number;
   area_name: string;
+  area_id: string;
   expired: boolean;
   state: string;
+  note: string;
 }
