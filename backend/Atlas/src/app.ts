041e80bcc38296d25fe8f7488fbb41f9aa2f4328
UnlikeMars <37548459+UnlikeMars@users.noreply.github.com>
2020-06-30 15:52:34 +0200

    Replace localhost with 0.0.0.0 for gRPC server and split env

--- backend/Atlas/src/app.ts
+++ backend/Atlas/src/app.ts
@@ -5,11 +5,11 @@ import {IParkplatzServer, ParkplatzService} from "./proto/parkplatz_grpc_pb";
 import {ParkplatzServer} from "./server/GrpcServer";
 
 const restServer = new Server();
-console.log(`SERVER: REST listening on ${process.env.PORT || 8080}`);
-restServer.start(+process.env.PORT || 8080);
+console.log(`SERVER: REST listening on ${process.env.HTTP_PORT || 8080}`);
+restServer.start(+process.env.HTTP_PORT || 8080);
 
 const grpcServer = new grpc.Server();
 grpcServer.addService<IParkplatzServer>(ParkplatzService, new ParkplatzServer());
-console.log(`SERVER: gRPC listening on ${process.env.PORT || 50051}`);
-grpcServer.bind(`localhost:${process.env.PORT || 50051}`, grpc.ServerCredentials.createInsecure());
+console.log(`SERVER: gRPC listening on ${process.env.GRPC_PORT || 50051}`);
+grpcServer.bind(`0.0.0.0:${process.env.GRPC_PORT || 50051}`, grpc.ServerCredentials.createInsecure());
 grpcServer.start();
